52f2a85416b9a3ba95bc0d0b59b0620878e5104a
Ismaël MEHDID <ismaelmehdidwork@gmail.com>
2025-01-28 20:36:19 +0100

    add secure connection between the backend and the blockchain container by using nginx as a reverse proxy using ssl

--- srcs/backend/games/blockchain_score_storage/connection.py
+++ srcs/backend/games/blockchain_score_storage/connection.py
@@ -3,7 +3,9 @@ from games.blockchain_score_storage.settings import HTTP_ADDRESS
 
 def get_web3_instance():
     """Connect to the blockchain and return a Web3 instance."""
-    web3 = Web3(Web3.HTTPProvider(HTTP_ADDRESS))
+
+    # Don't use ssl certificate verification as we use a self signed
+    web3 = Web3(Web3.HTTPProvider(HTTP_ADDRESS, request_kwargs={'verify': False}))
     if not web3.is_connected():
         raise ConnectionError("Couldn't connect to the blockchain.")
     return web3
--- srcs/backend/games/blockchain_score_storage/settings.py
+++ srcs/backend/games/blockchain_score_storage/settings.py
@@ -1,9 +1,9 @@
 import socket
 
 # Configuration for blockchain connection
-HTTP_PORT = 8545
-RESOLVED_DNS = socket.gethostbyname('blockchain')  # Resolve blockchain container DNS
-HTTP_ADDRESS = f"http://{RESOLVED_DNS}:{HTTP_PORT}"
+HTTP_PORT = 443 # Port of the nginx web server using ssl which will redirected the request to the blockchain container
+RESOLVED_DNS = socket.gethostbyname('nginx')  # Resolve nginx webserv container DNS
+HTTP_ADDRESS = f"https://{RESOLVED_DNS}:{HTTP_PORT}/blockchain/"
 
 # Paths to the compiled contract files
 ABI_PATH = './games/blockchain_score_storage/build/TournamentScores.abi'
